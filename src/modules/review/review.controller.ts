ecac0b18d977ce6c5e715a4655c3537aac74a8f7
WXZVRD <forworknickworron@gmail.com>
2025-04-12 01:33:36 +0500

    Add: review module implementation

---
+++ src/modules/review/review.controller.ts
@@ -0,0 +1,28 @@
+import {Body, Controller, Get, Post} from "@nestjs/common";
+import {ReviewService} from "./review.service";
+import {ReviewEntity} from "./entity/review.entity";
+import {ReviewCreateDto} from "./dto/review-create.dto";
+
+interface IReviewController {
+    getHello(): string
+
+    create(reviewData: ReviewCreateDto): Promise<ReviewEntity>
+}
+
+@Controller('/review')
+export class ReviewController implements IReviewController{
+    constructor(
+        private readonly reviewService: ReviewService
+    ) {
+    }
+
+    @Post('/create')
+    create(@Body() reviewData: ReviewCreateDto): Promise<ReviewEntity> {
+        return this.reviewService.create(reviewData)
+    }
+
+    @Get()
+    getHello(): string {
+        return this.reviewService.getHello()
+    }
+}
