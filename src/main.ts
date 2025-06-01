2799b8b163b4246431f443176d3a8bbe88af793c
wxzvrd <forworknickworron@gmail.com>
2025-06-01 17:23:22 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ConsoleLogger, ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,14 @@ async function bootstrap() {
       colors: true,
     }),
   });
+
+  const config = new DocumentBuilder()
+      .setTitle('NestJS BlogApp')
+      .setDescription('API Description')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
--- src/modules/cloudinary/cloudinary.provider.ts
+++ src/modules/cloudinary/cloudinary.provider.ts
@@ -4,9 +4,6 @@ import {ConfigService} from "@nestjs/config";
 export const CloudinaryProvider = {
     provide: 'CLOUDINARY',
     useFactory: async (configService: ConfigService) => {
-        console.log(configService.get('CLOUDINARY_NAME'))
-        console.log(configService.get('CLOUDINARY_API_KEY'))
-        console.log(configService.get('CLOUDINARY_API_SECRET'))
         return cloudinary.config({
             cloud_name: configService.get<string>('CLOUDINARY_NAME'),
             api_key: configService.get<string>("CLOUDINARY_API_KEY"),
